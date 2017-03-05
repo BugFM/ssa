d99801dde90f1231b27b243c27d81ef44a35e59f
Jon <jongordonelliott@hotmail.co.uk>
2017-03-05 11:54:23 +0000

    finished deconstruct method

--- ssa.py
+++ ssa.py
@@ -1,17 +1,29 @@
+# s = secret, n = total number of pieces, k = minimum required
 def deconstruct(secret, n, k):
     values = []
     for i in range(n):
-        values.append((i+1, polynomial(i+1) + secret))
+        values.append((i+1, polynomial(i+1, k, secret)))
     return tuple(values)
 
 
-def polynomial(k):
-    return 0
+def return_coefficients(k):
+    # k-1 random values.
+    return [166, 94]
+
+
+def polynomial(d, k, secret):
+    coefficients = return_coefficients(k)
+    for i in range(len(coefficients)):
+        secret += coefficients[i] * d**(i+1)
+    return secret
 
 
 def reconstruct():
+    # lagrange basis polynomials
     return True
 
+
+# wiki sample
 print deconstruct(1234, 6, 3)
 
 
