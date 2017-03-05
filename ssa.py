de77bd1857b9047cc23dd43ebd10d8c42d43cd3b
Jon <jongordonelliott@hotmail.co.uk>
2017-03-05 15:11:21 +0000

    started on reconstruct function

--- ssa.py
+++ ssa.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 # s = secret, n = total number of pieces, k = minimum required
 def deconstruct(secret, n, k):
     values = []
@@ -7,7 +10,10 @@ def deconstruct(secret, n, k):
 
 
 def return_coefficients(k):
-    # k-1 random values.
+    coefficients = []
+    for i in range(k-1):
+        coefficients.append(randint(1, 100000))
+    # return coefficients
     return [166, 94]
 
 
@@ -18,12 +24,35 @@ def polynomial(d, k, secret):
     return secret
 
 
-def reconstruct():
-    # lagrange basis polynomials
+# can be any number of shards
+def calculate_lemma(i):
+    pass
+
+
+def reconstruct(*shards):
+    print 'size of polynomial: ', len(shards)
+    polynomials = []
+    lagrange_basis_polynomial(shards)
+
+    # extrapolate the values = x0, x1...xn
+    # find the polynomial
+    # find the coefficients and constant
+    # return the constant/secret
     return True
 
 
+def lagrange_basis_polynomial(*shards):
+    # calculate coefficients of polynomials - zeroth most element is the secret
+    # extract x0, x1...xn
+    x_values = []
+    for i in shards:
+        x_values.append(i[0])
+    print x_values
+    return x_values
+
 # wiki sample
 print deconstruct(1234, 6, 3)
+
+reconstruct((1, 2), (34, 4))
 
 
